fbfc08bdc3352c7f86b672bc6915c1d58f712334
Raciz <1530071@upv.edu.mx>
2018-05-26 14:25:33 -0500

    documentacion de los modelos

--- Unidad 2/Practica8/models/conexion.php
+++ Unidad 2/Practica8/models/conexion.php
@@ -1,11 +1,15 @@
 <?php
 
+//clase para realizar la conecion a la base de datos
 class conexion
 {
+    //funcion para retornar una conexion a la base de datos
     public function conectar()
     {
+        //creamos la conexion
         $conn = new PDO("mysql:host=localhost;dbname=Tutorias","root","");
         
+        //y la retornamos
         return $conn;
     }
 }
--- Unidad 2/Practica8/models/crud.php
+++ Unidad 2/Practica8/models/crud.php
@@ -1,90 +1,127 @@
 <?php
 require_once "conexion.php";
 
+//clase para realizar operaciones a la base de datos
 class CRUD
 {
+    //modelo para registrar carrera en la base de datos
     public function registroCarreraModel($data,$tabla)
     {
+        //se prepara la sentencia para realizar el insert
         $stmt = Conexion::conectar() -> prepare("INSERT INTO $tabla (nombre) VALUES (:nombre)");
 
+        //se realiza la asignacion de los datos a insertar
         $stmt -> bindParam(":nombre",$data,PDO::PARAM_STR);
 
+        //se ejecuta la sentencia
         if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
             return "success";
         }
         else
         {
+            //en caso de no ser asi nos retorna fail
             return "fail";
         }
 
+        //cerramos la conexion
         $stmt -> close();
     }
 
+    //modelo para obtener la informacion de las carreras registradas
     public function listaCarreraModel($tabla)
     {
+        //preparamos la consulta y la ejecutamos
         $stmt = Conexion::conectar() -> prepare("SELECT * FROM $tabla");
         $stmt -> execute();
 
+        //retornamos la informacion de la tabla
         return $stmt -> fetchAll();
 
+        //cerramos la conexion
         $stmt -> close();
     }
 
+    //modelo para borar una carrera de la base de datos
     public function deleteCarreraModel($data,$tabla)
     {
+        //preparamos la sentencia para realizar el delete
         $stmt = Conexion::conectar() -> prepare("DELETE FROM $tabla WHERE id = :id");
 
+        //se realiza la asignacion de los datos a eliminar
         $stmt -> bindParam(":id",$data,PDO::PARAM_INT);
 
+        //se ejecuta la sentencia
         if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
             return "success";
         }
         else
         {
+            //en caso de no ser asi nos retorna fail
             return "fail";
         }
 
+        //cerramos la conexion
         $stmt -> close();
     }
 
+    //modelo para modificar la informacion de una carrera
     public function editCarreraModel($data,$tabla)
     {
+        //preparamos la sentencia para realizar el select
         $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE id = :id");
+        
+        //se realiza la asignacion de los datos para la consulta
         $stmt->bindParam(":id", $data, PDO::PARAM_INT);	
+        
+        //se ejecuta la sentencia
         $stmt->execute();
-
+        
+        //retornamos la fila obtenida con el select
         return $stmt->fetch();
 
+        //cerramos la conexion
         $stmt->close();
     }
 
+    //modelo para modificar la informacion de una carrera registrada en la base de datos
     public function updateCarreraModel($data,$tabla)
     {
+        //preparamos la sentencia para realizar el update
         $stmt = Conexion::conectar()->prepare("UPDATE $tabla SET nombre = :nombre WHERE id = :id");
 
+        //se realiza la asignacion de los datos para el update
         $stmt -> bindParam(":nombre", $data["nombre"], PDO::PARAM_STR);
         $stmt -> bindParam(":id", $data["id"], PDO::PARAM_INT);
 
+        //se ejecuta la sentencia
         if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
             return "success";
         }
         else
         {
+            //en caso de no ser asi nos retorna fail
             return "fail";
         }
 
+        //cerramos la conexion
         $stmt->close();
     }
 
-    //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+    //modelo para registrar un maestro en la base de datos 
     public function registroMaestroModel($data,$tabla)
     {
+        //preparamos la sentencia para realizar el select
         $stmt = Conexion::conectar() -> prepare("INSERT INTO $tabla (num_empleado,carrera,nombre,email,password,superUser) VALUES (:num_empleado,:carrera,:nombre,:email,:password,:superUser)");
 
+        //se realiza la asignacion de los datos para la consulta
         $stmt -> bindParam(":num_empleado",$data["num_empleado"],PDO::PARAM_STR);
         $stmt -> bindParam(":carrera",$data["carrera"],PDO::PARAM_INT);
         $stmt -> bindParam(":nombre",$data["nombre"],PDO::PARAM_STR);
@@ -92,62 +129,87 @@ class CRUD
         $stmt -> bindParam(":password",$data["password"],PDO::PARAM_STR);
         $stmt -> bindParam(":superUser",$data["super"],PDO::PARAM_INT);
 
+        //se ejecuta la sentencia
         if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
             return "success";
         }
         else
         {
+            //en caso de no ser asi nos retorna fail
             return "fail";
         }
 
+        //cerramos la conexion
         $stmt -> close();
     }
 
+    //modelo para obtener la informacion de los maestro registrados
     public function listaMaestroModel($tabla1,$tabla2)
     {
+        //preparamos la consulta y la ejecutamos
         $stmt = Conexion::conectar() -> prepare("SELECT m.num_empleado,c.nombre as carrera,m.nombre,m.email,m.superUser FROM $tabla1 as m JOIN $tabla2 as c on m.carrera = c.id");
         $stmt -> execute();
 
+        //retornamos la informacion de la tabla
         return $stmt -> fetchAll();
 
+        //cerramos la conexion
         $stmt -> close();
     }
     
+    //modelo para borrar un maestro de la base de datos
     public function deleteMaestroModel($data,$tabla)
     {
+        //se prepara la consulta para realizar el delete
         $stmt = Conexion::conectar() -> prepare("DELETE FROM $tabla WHERE num_empleado = :id");
 
+        //se realiza la asignacion de los datos para el delete
         $stmt -> bindParam(":id",$data,PDO::PARAM_STR);
 
+        //se ejecuta la sentencia
         if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
             return "success";
         }
         else
         {
+            //en caso de no ser asi nos retorna fail
             return "fail";
         }
 
+        //cerramos la conexion
         $stmt -> close();
     }
 
+    //modelo para modificar la informacion de una carrera
     public function editMaestroModel($data,$tabla)
     {
+        //preparamos la sentencia para realizar el select
         $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE num_empleado = :id");
-		$stmt->bindParam(":id", $data, PDO::PARAM_STR);	
-		$stmt->execute();
+		
+        //se realiza la asignacion de los datos para la consulta
+        $stmt->bindParam(":id", $data, PDO::PARAM_STR);	
+		
+        //se ejecuta la sentencia
+        $stmt->execute();
+		
+        //retornamos la fila obtenida con el select
+        return $stmt->fetch();
 
-		return $stmt->fetch();
-
+        //cerramos la conexion
 		$stmt->close();
     }
 
-
+    //modelo para modificar la informacion de un maestro registrado en la base de datos
     public function updateMaestroModel($data,$tabla)
     {
+        //preparamos la sentencia para realizar el update
         $stmt = Conexion::conectar()->prepare("UPDATE $tabla SET carrera = :carrera, nombre = :nombre, email = :email, password = :password, superUser = :super WHERE num_empleado = :num_empleado");
         
+        //se realiza la asignacion de los datos para el update
 		$stmt -> bindParam(":num_empleado", $data["num_empleado"], PDO::PARAM_STR);
 		$stmt -> bindParam(":carrera", $data["carrera"], PDO::PARAM_INT);
 		$stmt -> bindParam(":nombre", $data["nombre"], PDO::PARAM_STR);
@@ -155,99 +217,137 @@ class CRUD
 		$stmt -> bindParam(":password", $data["password"], PDO::PARAM_STR);
 		$stmt -> bindParam(":super", $data["super"], PDO::PARAM_INT);
 
+        //se ejecuta la sentencia
 		if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
 			return "success";
 		}
 		else
         {
+            //en caso de no ser asi nos retorna fail
 			return "fail";
 		}
 
+        //cerramos la conexion
 		$stmt->close();
     }
     
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
+    //modelo para registrar un alumno en la base de datos
     public function registroAlumnoModel($data,$tabla)
     {
+        //se prepara la sentencia para realizar el insert
         $stmt = Conexion::conectar() -> prepare("INSERT INTO $tabla (matricula,nombre,carrera,tutor) VALUES (:matricula,:nombre,:carrera,:tutor)");
 
+        //se realiza la asignacion de los datos a insertar
         $stmt -> bindParam(":matricula",$data["matricula"],PDO::PARAM_INT);
         $stmt -> bindParam(":nombre",$data["nombre"],PDO::PARAM_STR);
         $stmt -> bindParam(":carrera",$data["carrera"],PDO::PARAM_INT);
         $stmt -> bindParam(":tutor",$data["tutor"],PDO::PARAM_STR);
 
+        //se ejecuta la sentencia
         if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
             return "success";
         }
         else
         {
-            print_r($stmt->errorInfo());
+            //en caso de no ser asi nos retorna fail
             return "fail";
         }
 
+        //cerramos la conexion
         $stmt -> close();
     }
 
+    //modelo para obtener la informacion de los alumnos registrados
     public function listaAlumnoModel($tabla1,$tabla2,$tabla3)
     {
+        //preparamos la consulta y la ejecutamos
         $stmt = Conexion::conectar() -> prepare("SELECT a.matricula,a.nombre,c.nombre as carrera, m.nombre as tutor FROM $tabla1 as a JOIN $tabla2 as c on a.carrera = c.id JOIN $tabla3 as m on a.tutor = m.num_empleado");
         $stmt -> execute();
+        
+        //retornamos la informacion de la tabla
         return $stmt -> fetchAll();
 
+        //cerramos la conexion
         $stmt -> close();
     }
    
+    
+    //modelo para borar un alumno de la base de datos
     public function deleteAlumnoModel($data,$tabla)
     {
+        //preparamos la sentencia para realizar el delete
         $stmt = Conexion::conectar() -> prepare("DELETE FROM $tabla WHERE matricula = :id");
 
+        //se realiza la asignacion de los datos a eliminar
         $stmt -> bindParam(":id",$data,PDO::PARAM_INT);
 
+        //se ejecuta la sentencia
         if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
             return "success";
         }
         else
         {
+            //en caso de no ser asi nos retorna fail
             return "fail";
         }
 
+        //cerramos la conexion
         $stmt -> close();
     }
 
+    //modelo para modificar la informacion de un alumno
     public function editAlumnoModel($data,$tabla)
     {
+        //preparamos la sentencia para realizar el select
         $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE matricula = :id");
+        
+        //se realiza la asignacion de los datos para la consulta
 		$stmt->bindParam(":id", $data, PDO::PARAM_INT);	
+        
+        //se ejecuta la sentencia
 		$stmt->execute();
 
+        //retornamos la fila obtenida con el select
 		return $stmt->fetch();
 
+        //cerramos la conexion
 		$stmt->close();
     }
 
 
+    //modelo para modificar la informacion de un alumno registrado en la base de datos
     public function updateAlumnoModel($data,$tabla)
     {
+        //preparamos la sentencia para realizar el update
         $stmt = Conexion::conectar()->prepare("UPDATE $tabla SET nombre = :nombre, carrera = :carrera, tutor = :tutor WHERE matricula = :matricula");
         
+        //se realiza la asignacion de los datos para el update
 		$stmt -> bindParam(":nombre", $data["nombre"], PDO::PARAM_STR);
 		$stmt -> bindParam(":carrera", $data["carrera"], PDO::PARAM_INT);
 		$stmt -> bindParam(":tutor", $data["tutor"], PDO::PARAM_STR);
 		$stmt -> bindParam(":matricula", $data["matricula"], PDO::PARAM_STR);
 
+        //se ejecuta la sentencia
 		if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
 			return "success";
 		}
 		else
         {
+            //en caso de no ser asi nos retorna fail
 			return "fail";
 		}
 
+        //cerramos la conexion
 		$stmt->close();
     }
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -285,13 +385,16 @@ class CRUD
         $stmt -> bindParam(":tutoria",$data["tutoria"],PDO::PARAM_STR);
         
 
+        //se ejecuta la sentencia
         if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
             return "success";
         }
         else
         {
             print_r($stmt->errorInfo());
+            //en caso de no ser asi nos retorna fail
             return "fail";
         }
 
@@ -300,9 +403,12 @@ class CRUD
     
     public function listaTutoriaMaestroModel($tabla1,$tabla2,$tabla3,$tutor)
     {
+        //preparamos la consulta y la ejecutamos
         $stmt = Conexion::conectar() -> prepare("SELECT t.id, a.nombre as alumno,m.nombre as tutor, t.fecha, t.hora, t.tipo, t.tutoria FROM $tabla1 as t JOIN $tabla2 as m on t.tutor = m.num_empleado JOIN $tabla3 as a on a.matricula = t.alumno WHERE m.num_empleado = :tutor");
         $stmt -> bindParam(":tutor",$tutor,PDO::PARAM_STR);
         $stmt -> execute();
+        
+        //retornamos la informacion de la tabla
         return $stmt -> fetchAll();
 
         $stmt -> close();
@@ -314,12 +420,15 @@ class CRUD
 
         $stmt -> bindParam(":id",$data,PDO::PARAM_INT);
 
+        //se ejecuta la sentencia
         if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
             return "success";
         }
         else
         {
+            //en caso de no ser asi nos retorna fail
             return "fail";
         }
 
@@ -346,12 +455,15 @@ class CRUD
 		$stmt -> bindParam(":tutoria", $data["tutoria"], PDO::PARAM_STR);
 		$stmt -> bindParam(":id", $data["id"], PDO::PARAM_INT);
 
+        //se ejecuta la sentencia
 		if($stmt -> execute())
         {
+            //si se ejecuto correctamente nos retorna success
 			return "success";
 		}
 		else
         {
+            //en caso de no ser asi nos retorna fail
 			return "fail";
 		}
 		$stmt->close();
@@ -359,8 +471,11 @@ class CRUD
     
     public function reporteTutoriaMaestroModel($tabla1,$tabla2,$tabla3)
     {
+        //preparamos la consulta y la ejecutamos
         $stmt = Conexion::conectar() -> prepare("SELECT t.id, a.nombre as alumno,m.nombre as tutor, t.fecha, t.hora, t.tipo, t.tutoria FROM $tabla1 as t JOIN $tabla2 as m on t.tutor = m.num_empleado JOIN $tabla3 as a on a.matricula = t.alumno");
         $stmt -> execute();
+        
+        //retornamos la informacion de la tabla
         return $stmt -> fetchAll();
 
         $stmt -> close();
