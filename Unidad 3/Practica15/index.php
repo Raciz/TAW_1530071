76a761f3d41e7cf32015f6380f906b77e6bc86eb
Raciz <1530071@upv.edu.mx>
2018-07-09 10:54:44 -0500

    update

--- Unidad 3/Practica15/index.php
+++ Unidad 3/Practica15/index.php
@@ -3,6 +3,7 @@
 //incluimos en esta seccion todo lo nesesario para el funcionamiento del sitio
 require_once("controllers/controller.php");
 require_once("models/crud.php");
+require_once("models/url.php");
 
 //creamos un objeto de mvcController
 $MVC = new mvcController();
--- Unidad 3/Practica15/views/template.php
+++ Unidad 3/Practica15/views/template.php
@@ -230,54 +230,20 @@ session_start();
                         </div>
                     </div>
                     <!--Scrollbar wrapper-->
-                    
+
                 </aside>
                 <!--left navigation end-->
 
                 <!-- START PAGE CONTENT -->
                 <div id="page-right-content">
 
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="card-box widget-inline">
-                                    <div class="row">
-                                        <div class="col-lg-3 col-sm-6">
-                                            <div class="widget-inline-box text-center">
-                                                <h3 class="m-t-10"><i class="text-primary mdi mdi-access-point-network"></i> <b data-plugin="counterup">8954</b></h3>
-                                                <p class="text-muted">Lifetime total sales</p>
-                                            </div>
-                                        </div>
+                    <?php
+                    //creamos un objeto de mvcController
+                    $mvc = new mvcController();
+                    //y obtenemos el controlador para el redireccionamiento
+                    $mvc -> urlController();
+                    ?>
 
-                                        <div class="col-lg-3 col-sm-6">
-                                            <div class="widget-inline-box text-center">
-                                                <h3 class="m-t-10"><i class="text-custom mdi mdi-airplay"></i> <b data-plugin="counterup">7841</b></h3>
-                                                <p class="text-muted">Income amounts</p>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-lg-3 col-sm-6">
-                                            <div class="widget-inline-box text-center">
-                                                <h3 class="m-t-10"><i class="text-info mdi mdi-black-mesa"></i> <b data-plugin="counterup">6521</b></h3>
-                                                <p class="text-muted">Total users</p>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-lg-3 col-sm-6">
-                                            <div class="widget-inline-box text-center b-0">
-                                                <h3 class="m-t-10"><i class="text-danger mdi mdi-cellphone-link"></i> <b data-plugin="counterup">325</b></h3>
-                                                <p class="text-muted">Total visits</p>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!--end row -->
-                    </div>
-                    <!-- end container -->
-                    
                     <div class="footer">
                         <div>
                             <strong>Angela Carrizales, Brian Becerra y Francisco Perales</strong> - Copyright &copy; 2018
